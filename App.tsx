c3d57cad51e7245c47575fb1f1a8e516a72c883f
Hubert Ryan <hubertryanofficial@gmail.com>
2024-05-05 04:42:38 -0300

    feat: sora font regular and semibold

--- App.tsx
+++ App.tsx
@@ -1,5 +1,32 @@
+import { useCallback } from "react";
+import { View } from "react-native";
+
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
 import Routes from "./src/routes/index.routes";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
-  return <Routes />;
+  const [fontsLoaded, fontError] = useFonts({
+    "Sora-Regular": require("./src/assets/fonts/Sora-Regular.ttf"),
+    "Sora-SemiBold": require("./src/assets/fonts/Sora-SemiBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <View className="flex-1" onLayout={onLayoutRootView}>
+      <Routes />
+    </View>
+  );
 }
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Image, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 import HeaderImage from "@/assets/header-img.png";
 
 const Login: React.FC = () => {
   return (
     <View className="flex-1 bg-purple-heart">
-      <View>
+      <View className="w-full justify-center items-center pt-24 pb-12">
         <Image source={HeaderImage} />
+      </View>
+      <View className="rounded-t-3xl bg-alabaster flex-1">
+        <Text className="text-3xl text-mine-shaft self-center mt-8">Login</Text>
       </View>
     </View>
   );
