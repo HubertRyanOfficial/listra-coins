c31d4050c2703fe99c3078a3dfcb05399dc166f5
Hubert Ryan <hubertryanofficial@gmail.com>
2024-05-05 04:26:27 -0300

    fix: declarations to images

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { View } from "react-native";
+import { Image, View } from "react-native";
+
+import HeaderImage from "@/assets/header-img.png";
 
 const Login: React.FC = () => {
-  return <View className="flex-1 bg-purple-heart" />;
+  return (
+    <View className="flex-1 bg-purple-heart">
+      <View>
+        <Image source={HeaderImage} />
+      </View>
+    </View>
+  );
 };
 
 export default Login;
