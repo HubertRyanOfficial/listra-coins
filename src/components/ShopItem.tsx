4e3a5bb28c75d88500046c8a659c87445fdaa81e
Hubert Ryan <hubertryanofficial@gmail.com>
2024-05-06 17:36:09 -0300

    feat: expo sharing to products

--- src/components/ShopItem.tsx
+++ src/components/ShopItem.tsx
@@ -8,6 +8,8 @@ import {
   ActivityIndicator,
 } from "react-native";
 
+import * as Sharing from "expo-sharing";
+
 import { Product } from "@/services/products/types";
 import { addToCart } from "@/services/Shopping";
 
@@ -54,48 +56,59 @@ export default function ShopItem({ data, userId }: Props) {
     }
   }, [inCart]);
 
+  const handleShare = () => async () => {
+    const isAvailable = await Sharing.isAvailableAsync();
+    if (isAvailable) {
+      Sharing.shareAsync(data.image, {
+        dialogTitle: `Listra Coins - ${data.name}`,
+      });
+    }
+  };
+
   return (
-    <View
-      className="bg-white self-start rounded-2xl shadow"
-      style={{
-        width: CARD_WIDTH,
-        height: CARD_HEIGHT,
-      }}
-    >
-      <Image
-        source={{
-          uri: data.image,
+    <TouchableOpacity delayLongPress={250} onLongPress={handleShare}>
+      <View
+        className="bg-white self-start rounded-2xl shadow"
+        style={{
+          width: CARD_WIDTH,
+          height: CARD_HEIGHT,
         }}
-        className="rounded-t-2xl"
-        style={{ width: CARD_WIDTH, height: CARD_HEIGHT / 2 - 10 }}
-      />
-      <View className="p-4">
-        <Text className="font-soraSemibold text-base" numberOfLines={1}>
-          {data.name}
-        </Text>
-        <Text className="font-sora text-xs text-alabaster-300 mt-1">
-          {data.units} unidades
-        </Text>
-        <View className="pl-2 mt-3.5 flex-row items-center justify-between">
-          <View>
-            <Text className="font-sora text-xs text-purple-heart">Lc</Text>
-            <Text className="font-soraSemibold text-lg text-purple-heart -mt-1">
-              {data.price.toFixed(2)}
-            </Text>
-          </View>
-          <TouchableOpacity onPress={handleAddInCart} disabled={inCart}>
-            <View className="bg-purple-heart w-[33px] h-[33px] justify-center items-center rounded-xl">
-              {loading && !inCart ? (
-                <ActivityIndicator color="#FFFFFF" />
-              ) : !inCart ? (
-                <ShopCartAddIcon width={18} height={18} color="#FFFFFF" />
-              ) : (
-                <CheckCircleIcon width={18} height={18} />
-              )}
+      >
+        <Image
+          source={{
+            uri: data.image,
+          }}
+          className="rounded-t-2xl"
+          style={{ width: CARD_WIDTH, height: CARD_HEIGHT / 2 - 10 }}
+        />
+        <View className="p-4">
+          <Text className="font-soraSemibold text-base" numberOfLines={1}>
+            {data.name}
+          </Text>
+          <Text className="font-sora text-xs text-alabaster-300 mt-1">
+            {data.units} unidades
+          </Text>
+          <View className="pl-2 mt-3.5 flex-row items-center justify-between">
+            <View>
+              <Text className="font-sora text-xs text-purple-heart">Lc</Text>
+              <Text className="font-soraSemibold text-lg text-purple-heart -mt-1">
+                {data.price.toFixed(2)}
+              </Text>
             </View>
-          </TouchableOpacity>
+            <TouchableOpacity onPress={handleAddInCart} disabled={inCart}>
+              <View className="bg-purple-heart w-[33px] h-[33px] justify-center items-center rounded-xl">
+                {loading && !inCart ? (
+                  <ActivityIndicator color="#FFFFFF" />
+                ) : !inCart ? (
+                  <ShopCartAddIcon width={18} height={18} color="#FFFFFF" />
+                ) : (
+                  <CheckCircleIcon width={18} height={18} />
+                )}
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
